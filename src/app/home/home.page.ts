8c6e620c0d50cbd913a674ba928d97dfe79b9b46
Caroline Roy <caroline.roy.tln@gmail.com>
2021-02-02 14:36:44 +0100

    Init projet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,6 @@
+import { CacheService } from 'ionic-cache';
+
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HttpClient, private cache: CacheService) {}
+  public apiData: any = null;
 
+  ionViewDidEnter(){
+    this.getData();
+
+  }
+
+  getData(){
+    const URL = "https://picsum.photos/v2/list?limit=10";
+    //  this.http.get(URL).subscribe((data)=>{
+    //    console.log(data);
+    //  })
+
+    const request = this.http.get(URL);
+    this.cache.loadFromObservable('myCacheKey', request).subscribe((data)=>{
+      console.log("Data", data);
+      this.apiData= data;
+    })
+  }
 }
